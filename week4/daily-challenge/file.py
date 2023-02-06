5974e56fd4d7f40e20e7b6760d38c58b02f8f396
Hemlesh18 <hemlesh@icloud.com>
2023-02-07 00:09:36 +0400

    dailychallenge

---
+++ week4/daily-challenge/file.py
@@ -0,0 +1,18 @@
+sentence = input("Enter a string of 10 characters long:")
+if len(sentence) < 10:
+    print("String not long enough")
+elif len(sentence) > 10:
+    print("string too long")
+else:
+    print(f"{sentence[0]}\n {sentence[-1]}")
+
+result = " "
+for x in sentence:
+    result += x
+    print(result)
+
+import random
+str_list = list(sentence)
+random.shuffle(str_list)
+str_final=''.join(str_list)
+print(str_final)
