bc5a3538c9e30e1bd2363ad6ba3c550a54c6c811
Hemlesh18 <hemlesh@icloud.com>
2023-02-06 23:36:41 +0400

    python1

--- week4/exercise/xp/exercise.py
+++ week4/exercise/xp/exercise.py
@@ -1,57 +1,57 @@
-# ex1
-# print("hello world\n"*4)
+ex1
+print("hello world\n"*4)
 
-# ex2
-# print((99^3)*8)
+ex2
+print((99^3)*8)
 
-# ex4
-# computer_brand = ("asus")
-# print("i have a " +computer_brand +" computer")
+ex4
+computer_brand = ("asus")
+print("i have a " +computer_brand +" computer")
 
-# ex5
-# name = ("khavind")
-# age =str(21)
-# shoe_size=str(42)
-# info=("I name is "+name+"," "i am "+age+" year old." " My shoe size is "+shoe_size)
-# print(info)
+ex5
+name = ("khavind")
+age =str(21)
+shoe_size=str(42)
+info=("I name is "+name+"," "i am "+age+" year old." " My shoe size is "+shoe_size)
+print(info)
 
-# ex6
-# a=(2)
-# b=(10)
-# if a > b:
-#     print("hello world")
-# # else:
-# #     print("")
-
-
-# ex7
-# num=int(input("enter a number:"))
-# if (num%2)==0:
-#     print("number is even")
+ex6
+a=(2)
+b=(10)
+if a > b:
+    print("hello world")
 # else:
-#     print("number is odd")
+#     print("")
 
-# ex8
-# user_name= input("enter your name")
-# my_name=("khavind")
-# if user_name== my_name:
-#     print("balalala we have the same name")
-# else:
-#     print()
 
-# ex9
-# user_height=float((input("enter your height in inches")))
-# height_cm=(user_height*2.54)
-# if height_cm > 145:
-#     print("you are told enough to ride")
-# elif height_cm < 145:
-#     print(" grow some more to ride.")
-# else:
-#     print("error. accept only height in inch")
+ex7
+num=int(input("enter a number:"))
+if (num%2)==0:
+    print("number is even")
+else:
+    print("number is odd")
 
-# ex3
-#  5 < 3  false
-#  3 == 3 true
-#  3 == "3"  false
-#  "3" > 3 false
-#  "Hello" == "hello" fales
+ex8
+user_name= input("enter your name")
+my_name=("khavind")
+if user_name== my_name:
+    print("balalala we have the same name")
+else:
+    print()
+
+ex9
+user_height=float((input("enter your height in inches")))
+height_cm=(user_height*2.54)
+if height_cm > 145:
+    print("you are told enough to ride")
+elif height_cm < 145:
+    print(" grow some more to ride.")
+else:
+    print("error. accept only height in inch")
+
+ex3
+ 5 < 3  false
+ 3 == 3 true
+ 3 == "3"  false
+ "3" > 3 false
+ "Hello" == "hello" fales
