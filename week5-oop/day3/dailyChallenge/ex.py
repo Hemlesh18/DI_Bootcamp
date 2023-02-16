40160132bdcb965588c4e94ffb6a29e5d075e384
Hemlesh18 <hemlesh@icloud.com>
2023-02-16 23:00:11 +0400

    day3DC

---
+++ week5-oop/day3/dailyChallenge/ex.py
@@ -0,0 +1,26 @@
+import math
+class Circle():
+    def __init__(self,radius):
+        self.radius= radius
+    def area(self):
+        return math.pi*self.radius**2
+    def __add__(self, other):
+        return self.radius + other.radius
+    def __gt__(self,other):
+        return self.radius > other.radius
+    def sort(self,other):
+        list=[]
+        list.append(int(self.radius))
+        list.append(int(other.radius))
+        list = sorted(list)
+        return list
+    
+circle1 = Circle(14)
+Circle2 = Circle(8)
+print(f" Area of circle is {circle1.area()}")
+addition =circle1 +Circle2
+print(f"sum is {addition}")
+print(circle1>Circle2)
+print(circle1< Circle2)
+print(circle1.sort(Circle2))
+    
