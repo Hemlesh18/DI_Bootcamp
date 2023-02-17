aa03a5f2418ae7e819350c2f8eecf130533b1470
Hemlesh18 <hemlesh@icloud.com>
2023-02-17 16:31:38 +0400

    day5 week5

---
+++ week5-oop/day5/dailyChallenge/oopQuizz.py
@@ -0,0 +1,62 @@
+# Part 1 : Quizz :
+# Answer the following questions
+
+# What is a class?
+# A class is a template/blueprint for creating object
+
+# What is an instance?
+# to reuse the field and method of existing file
+
+# What is encapsulation?
+# it is a way to restrict the dirrect acces to some components of an object
+
+# What is abstraction?
+# is a process of handling complexity by hiding unnecessary information from the user
+
+# What is inheritance?
+# It is the procedure in which one class inherits the attributes and methods of another class
+
+# What is multiple inheritance?
+# a class that derived  form more than base class 
+
+# What is polymorphism?
+# Polymorphism is a programming principle that allows for the same code or method to be used for multiple different objects.
+
+# What is method resolution order or MRO?
+# Method resolution order (MRO) is the algorithm used by Python to determine the order in which methods should be inherited from parent classes.
+
+
+
+# part2 
+
+# Create A Deck Of Cards Class.
+# The Deck of cards class should NOT inherit from a Card class.
+
+# The requirements are as follows:
+
+# The Card class should have a suit (Hearts, Diamonds, Clubs, Spades) and a value (A,2,3,4,5,6,7,8,9,10,J,Q,K)
+# The Deck class :
+# should have a shuffle method which makes sure the deck of cards has all 52 cards and then rearranges them randomly.
+# should have a method called deal which deals a single card from the deck. After a card is dealt, it should be removed from the deck.
+
+import itertools, random
+
+class Card:
+
+    deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
+    random.shuffle(deck)
+
+    print("You got:")
+    for i in range(1):
+        print(deck[i][0], "of", deck[i][1])
+
+deck = Card()
+
+
+
+
+
+
+
+
+        
